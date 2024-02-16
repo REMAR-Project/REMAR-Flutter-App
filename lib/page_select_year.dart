d1fad5b4e975753985ec0aed048461df8353f950
Tim Honisett <40595819@live.napier.ac.uk>
2024-02-16 18:22:24 +0000

    Added sub header crab image and name.  Added instructional text

--- lib/page_select_year.dart
+++ lib/page_select_year.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
 import 'app_bar.dart';
 import 'bottom_navigation_bar.dart';
+import 'Strings_english.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: CustomAppBar(),
-        bottomNavigationBar: CustomBottomNavigationBar(),
+        appBar: const CustomAppBar(),
+        bottomNavigationBar: const CustomBottomNavigationBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/raster_q0_0.png',
+                  width: 100,
+                  height: 75,
+                ),
+                const Text(
+                  Strings.monitoring_Cardisoma_guanhumi,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 350,
+                child: Text(Strings.choose_year_instruction,
+              style: TextStyle(fontSize: 15),
+              )
+            ),
+          ],
+        ),
+
       ),
     );
+
   }
 }
+
+
