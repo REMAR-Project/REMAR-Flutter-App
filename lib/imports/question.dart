fe4147d48b1c599c84bb8a2a8f7e6d2328572512
ReduxGB <alex@reduxgb.com>
2024-03-03 22:04:57 +0000

    Adds ability to import questions json as array

---
+++ lib/imports/question.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class Question {
+  final int questionNumber;
+  final String questionType;
+  final String questionPicture;
+  final String questionTitle;
+  final String questionText;
+  final String year;
+
+  Question({required this.questionNumber, required this.questionType, required this.questionPicture, required this.questionTitle, required this.questionText, required this.year});
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      questionNumber: json['questionNumber'] ?? '',
+      questionType: json['questionType'] ?? '',
+      questionPicture: json['questionPicture'] ?? '',
+      questionTitle: json['questionTitle'] ?? '',
+      questionText: json['questionText'] ?? '',
+      year: json['year'] ?? '',
+    );
+  }
+}
+
+Future<List<Question>> fetchQuestions() async {
+  String jsonString = await rootBundle.loadString('raw_eng/questions.json');
+  List<dynamic> json = jsonDecode(jsonString);
+  return json.map((question) => Question.fromJson(question)).toList();
+}
