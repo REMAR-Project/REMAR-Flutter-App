172baa70aa8cd8fbfbbc2569d19b2347efd7aaa3
ReduxGB <alex@reduxgb.com>
2024-03-03 22:05:20 +0000

    Question view to navigate through pages

---
+++ lib/questionpage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:remar_flutter_app/imports/question.dart';
+import 'package:remar_flutter_app/imports/widgetImports.dart';
+import 'package:remar_flutter_app/widgets/app_bar.dart';
+import 'package:remar_flutter_app/widgets/bottom_navigation_bar.dart';
+
+class QuestionPage extends StatefulWidget {
+  @override
+  _QuestionPageState createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  int currentQuestionNumber = 1;
+  List<Question> questions = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: FutureBuilder<List<Question>>(
+        future: fetchQuestions(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            questions = snapshot.data!;
+            Question currentQuestion = questions.firstWhere((question) => question.questionNumber == currentQuestionNumber);
+            // TODO: Add other question types properly
+            switch (currentQuestion.questionType) {
+              case 'YearChoice':
+                return const MoonCalendar();
+              case 'MonthChoice':
+                return const MoonCalendar();
+              default:
+                return MoonCalendar();
+            }
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+      bottomNavigationBar: questions != null ? CustomBottomNavigationBar(
+        onNext: () {
+          if (currentQuestionNumber < questions.length) {
+            setState(() {
+              currentQuestionNumber++;
+            });
+          }
+        },
+        onPrevious: () {
+          if (currentQuestionNumber > 1) {
+            setState(() {
+              currentQuestionNumber--;
+            });
+          }
+        },
+        currentPage: currentQuestionNumber,
+        totalPages: questions.length,
+      ) : null,
+    );
+  }
+}
