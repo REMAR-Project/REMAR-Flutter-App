44b419e3b9fc5c4ab0589b33cbc14027f41b70b3
ReduxGB <alex@reduxgb.com>
2024-03-03 22:04:25 +0000

    Add navigation button to questions view (WIP)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'app_bar.dart';
-import 'bottom_navigation_bar.dart';
+import 'widgets/app_bar.dart';
+import 'questionpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +10,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: CustomAppBar(),
-        bottomNavigationBar: CustomBottomNavigationBar(),
+      home: Builder(
+        builder: (context) => Scaffold(
+          appBar: const CustomAppBar(),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QuestionPage()),
+                );
+              },
+              child: Text('Go to QuestionPage'),
+            ),
+          ),
+        ),
       ),
     );
   }
