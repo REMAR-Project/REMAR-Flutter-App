5fdaec323076212885514eeaa4d756f5c37c97bc
ReduxGB <alex@reduxgb.com>
2024-03-25 05:32:47 +0000

    File Rename, add new calendar screen

--- lib/imports/widget_imports.dart
+++ lib/imports/widget_imports.dart
@@ -1,3 +1,3 @@
-export 'package:remar_flutter_app/widgets/moon_calendar.dart';
+export 'package:remar_flutter_app/widgets/calendar_screen.dart';
 export 'package:remar_flutter_app/widgets/list_selection.dart';
 export 'package:remar_flutter_app/widgets/year_month_selection.dart';
