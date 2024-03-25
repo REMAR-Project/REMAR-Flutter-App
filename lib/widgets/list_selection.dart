ad995518369ea5d64fc555fad14df489e6afe282
ReduxGB <alex@reduxgb.com>
2024-03-25 07:13:55 +0000

    Minor changes to list_selection

--- lib/widgets/list_selection.dart
+++ lib/widgets/list_selection.dart
@@ -22,34 +22,30 @@ class _ListSelectionState extends State<ListSelection> {
   }
 
 void readJson() async {
-  print('Loading asset: assets/raw_eng/${widget.fileName}');
   final String response = await rootBundle.loadString('assets/raw_eng/${widget.fileName}');
   final data = await json.decode(response);
-  print('Parsed data: $data');
   setState(() {
     items = List<String>.from(data);
   });
 }
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Center(
-                child: Text(items[index]),
-              ),
-              onTap: () {
-                print('Item clicked: ${items[index]}');
-                widget.onSelection(items[index]);
-              },
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Center(
+              child: Text(items[index]),
             ),
-          );
-        },
-      ),
+            onTap: () {
+              widget.onSelection(items[index]);
+            },
+          ),
+        );
+      },
     );
   }
+
 }
