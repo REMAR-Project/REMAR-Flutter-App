e2e288ac82680e130c36707a057424af87ce34ac
Tim Honisett <40595819@live.napier.ac.uk>
2024-04-07 20:59:04 +0100

    Code base from Tayyab now in this feature branch. Runs with issues on first page

---
+++ lib/main..dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:remar_flutter_app/Screens/splash_page.dart';
+import 'package:remar_flutter_app/utils/color_res.dart';
+import 'Screens/providers/bottom_state_provider.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => BottomStateProvider())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: ColorRes.greenColor,
+          canvasColor: Colors.white,
+          textTheme: GoogleFonts.latoTextTheme(),
+          useMaterial3: true,
+        ),
+        home: const SplashPage(),
+        // home:  QuestionAnswer3Page(image: crabImage, name: crabName),
+      ),
+    );
+  }
+}
