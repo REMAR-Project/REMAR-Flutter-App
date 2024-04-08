ef577021183eacc3e557be13c8742ba818087c9b
Tim Honisett <tim.honisett@hotmail.co.uk>
2024-04-08 18:04:50 +0100

    feature-taylab-new-pages-April (#58)
    * Initial Commit
    
    * Implements top app bar
    
    * Layout for bottom navigation bar
    
    * Top app bar
    
    * General Page Layout
    
    * Had build error due to "const" constructor. error resolved
    
    * Added the JSON files to the repo which consists of all questions along with other information displayed in the app.
    
    * Added the JSON files to the repo which consists of all questions along with other information displayed in the app.
    
    * changed bottom navigation bar so the number of pages currently user is in gets displayed correctly.
    
    * question 10 page, uses string questions intead of directly fetching json. ongoing tasks.
    
    * fixed the images loading issue, where the image couldnt be fetched previously.
    
    * question 8 and 9 using json data.
    
    * fixed the issue with "question" being fetched from the wrong questionnaire in the json data.
    
    * fixed both json data loading as there was an error wher question 9 was loading question 8 data. fixed.
    
    * app navigation can be done now, since i have 2 pages i am working on, user can navigate back and forth between the pages now and the bottom bar will change accordingly.
    
    * first commit
    
    * first commit
    
    * basic screen working
    
    * Updated elements for sizing on screen 3
    
    * Updated elements for sizing on screen 4
    
    * Updated question7.dart
    
    * question8.dart pulling county list based on hard coded state
    
    * Pass state, crab name and image file to question8.dart
    
    * Pass crab name and image file to question3.dart
    
    * Pass crab name and image file to question4.dart
    
    * Data now passed to Q3, Q4, Q7, Q8
    
    * Screens refactored to work with amended modifiedquestions JSON
    
    * Screens refactored to work with amended modified questions JSON. Bottom navigation restored to original format
    
    * Code base from Tayyab now in this feature branch. Runs with issues on first page
    
    * Code base from Tayyab now in this feature branch. Runs with issues on first page
    
    * Added all image assets from original Android app
    
    * Pass data back from page3.  Returned data variables defined
    
    * Pass data back from page4.
    
    * Page 4 listbox size fixed for web
    
    * Refactored questions_utils.dart
    
    * Refactored questions_utils.dart
    
    * question6.dart page added and linked
    
    * question6.dart page added and linked
    
    * question6.dart page added and linked
    
    * question6.dart page added and linked
    
    * question6.dart page added and linked
    
    * Revert "question6.dart page added and linked"
    
    This reverts commit 6dc797d08a9a7f9f09a7e013b308ab06d86158af.
    
    * Page 6 added
    
    * question9.dart added
    
    * added question6.dart
    
    * added question6.dart
    
    * Selected state now passed to question12 to display correct counties
    
    ---------
    
    Co-authored-by: Redux GB <alex@reduxgb.com>
    Co-authored-by: Adarsha Aryal <109141779+findingaadi@users.noreply.github.com>
    Co-authored-by: findingaadi <adarshaaryal30@gmail.com>
    Co-authored-by: Alex <alex@alexis.scot>
    Co-authored-by: Tim Honisett <40595819@live.napier.ac.uk>
    Co-authored-by: tayyabcreator <123637905+tayyabcreator@users.noreply.github.com>

---
+++ lib/bottom_navigation_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentPageIndex;
+  final Function(int) onPageChanged;
+
+  const CustomBottomNavigationBar({
+    Key? key,
+    required this.currentPageIndex,
+    required this.onPageChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              onPageChanged(currentPageIndex - 1); // Go to previous page
+            },
+          ),
+          Text('Page ${currentPageIndex + 1} of 2'),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward),
+            onPressed: () {
+              onPageChanged(currentPageIndex + 1); // Go to next page
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
