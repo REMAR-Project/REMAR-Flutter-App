76b5a475515abfdaba5e9889ccd68460ad23478f
Tim Honisett <tim.honisett@hotmail.co.uk>
2024-04-19 19:22:34 +0100

    Update widget_test.dart
    fix merge issues

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:remar_flutter_app/main.dart';
-import 'package:remar_flutter_app/main..dart';
 
 
 
