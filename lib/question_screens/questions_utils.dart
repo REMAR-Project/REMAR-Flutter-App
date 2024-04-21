c0b877ec8115fa5df0790677132d1c4dd7543f9d
Tim Honisett <40595819@live.napier.ac.uk>
2024-04-21 15:22:42 +0100

    Short questions now added to questions_utils.dart

--- lib/question_screens/questions_utils.dart
+++ lib/question_screens/questions_utils.dart
@@ -33,14 +33,23 @@ List<Widget> questionsPagesLongList() {
     const QuestionAnswer12Page(),
     const QuestionAnswer13Page(),
     const QuestionAnswer14Page(),
-   const QuestionAnswer15Page(),
+    const QuestionAnswer15Page(),
+    const QuestionAnswer16Page()
 
   ];
 }
 
 List<Widget> questionsPagesShortList() {
   return [
-
+    const QuestionAnswer2Page(),
+    const QuestionAnswer3Page(),
+    const QuestionAnswer4Page(),
+    const QuestionAnswer7Page(),
+    const QuestionAnswer11Page(),
+    const QuestionAnswer12Page(),
+    const QuestionAnswer13Page(),
+    const QuestionAnswer14Page(),
+    const QuestionAnswer15Page(),
   ];
 }
 
