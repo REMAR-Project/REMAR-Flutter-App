410479e8962e10fae32cce392f6dcafd8ec0d211
Tim Honisett <40595819@live.napier.ac.uk>
2024-04-21 15:28:44 +0100

    Slight refactoring of questions_utils.dart

--- lib/question_screens/questions_utils.dart
+++ lib/question_screens/questions_utils.dart
@@ -2,10 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:remar_flutter_app/question_screens/question2.dart';
 import 'package:remar_flutter_app/question_screens/question3.dart';
 import 'package:remar_flutter_app/question_screens/question4.dart';
-import 'package:remar_flutter_app/question_screens/question5.dart';
 import 'package:remar_flutter_app/question_screens/question7.dart';
 import 'package:remar_flutter_app/question_screens/question8.dart';
-//import 'package:remar_flutter_app/question_screens/question8.dart';
 import 'package:remar_flutter_app/question_screens/question9.dart';
 import 'package:remar_flutter_app/question_screens/question10.dart';
 import 'package:remar_flutter_app/question_screens/question11.dart';
@@ -13,11 +11,6 @@ import 'package:remar_flutter_app/question_screens/question12.dart';
 import 'package:remar_flutter_app/question_screens/question13.dart';
 import 'package:remar_flutter_app/question_screens/question14.dart';
 import 'package:remar_flutter_app/question_screens/question15.dart';
-import 'package:remar_flutter_app/question_screens/question16.dart';
-
-
-
-
 
 
 List<Widget> questionsPagesLongList() {
@@ -40,8 +33,17 @@ List<Widget> questionsPagesLongList() {
 
 List<Widget> questionsPagesShortList() {
   return [
-
+    const QuestionAnswer2Page(),
+    const QuestionAnswer3Page(),
+    const QuestionAnswer4Page(),
+    const QuestionAnswer7Page(),
+    const QuestionAnswer11Page(),
+    const QuestionAnswer12Page(),
+    const QuestionAnswer13Page(),
+    const QuestionAnswer14Page(),
+    const QuestionAnswer15Page(),
   ];
 }
+
 
 
