facaccb5914915f3e62d9a6425f953830b5f92b0
Tim Honisett <tim.honisett@hotmail.co.uk>
2024-04-22 10:12:15 +0100

    Refinement 22 april (#105)
    * Able to navigate back to question15.dart when selecting changing answers from question16.dart. New commit
    
    * Able to navigate back to question15.dart when selecting changing answers from question16.dart. New commit
    
    ---------
    
    Co-authored-by: Tim Honisett <40595819@live.napier.ac.uk>

--- lib/question_screens/question16.dart
+++ lib/question_screens/question16.dart
@@ -4,6 +4,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:remar_flutter_app/global.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:remar_flutter_app/question_screens/question15.dart';
 
 class QuestionAnswer16Page extends StatefulWidget {
   const QuestionAnswer16Page({Key? key}) : super(key: key);
@@ -107,6 +108,12 @@ class _QuestionAnswer16PageState extends State<QuestionAnswer16Page> {
         onPressed: () {
           setState(() {
             selectedAnswer = title;
+            finalPageResponse = selectedAnswer;
+            if (selectedAnswer == "I WANT TO GO BACK TO CHANGE ANSWERS") {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const QuestionAnswer15Page()),
+              );
+            }
           });
         },
         style: ElevatedButton.styleFrom(
