78065182f6882ce5d54d1f7b529023dc67897510
Tim Honisett <tim.honisett@hotmail.co.uk>
2024-04-29 18:07:58 +0100

    Fixed issue where forward navigation can happen if yes is selected in protected area, even though an area was not selected (#123)
    Co-authored-by: Tim Honisett <40595819@live.napier.ac.uk>

--- lib/question_screens/question13.dart
+++ lib/question_screens/question13.dart
@@ -131,6 +131,7 @@ class _QuestionAnswerPage13State extends State<QuestionAnswer13Page> {
                 isExpanded = true;
                 selectedArea = answers as String;
                 protectedArea = answers as String;
+
                 enableForwardNavigation = true;
                 backwardsNavigation = false;
               });
@@ -152,7 +153,13 @@ class _QuestionAnswerPage13State extends State<QuestionAnswer13Page> {
           // Update the flag based on the selected answer
           displayAnswers = selectedAnswer == "Yes";
           protectedAreaResponse = title;
-          enableForwardNavigation = true;
+
+          if(selectedAnswer=="No" || selectedAnswer=="I don't know") {
+            enableForwardNavigation = true;
+          }
+
+
+
         });
       },
       child: Row(
@@ -185,6 +192,9 @@ class _QuestionAnswerPage13State extends State<QuestionAnswer13Page> {
         setState(() {
           selectedArea = answer;
           protectedArea = selectedArea;
+          if(selectedAnswer=="Yes" && selectedAnswer!="") {
+            enableForwardNavigation = true;
+          }
 
         });
       },
